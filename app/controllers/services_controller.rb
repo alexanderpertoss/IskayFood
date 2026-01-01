b5efa271512d15b3d2f71aa691f5e0a0cb4c6ff1
alexanderpertoss <alexgunner2012@gmail.com>
2026-01-01 14:39:04 -0400

    added pages for services and products

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,6 @@
 class ServicesController < ApplicationController
   before_action :set_service, only: %i[ show edit update destroy ]
+  allow_unauthenticated_access only: %i[ show ]
 
   # GET /services or /services.json
   def index
